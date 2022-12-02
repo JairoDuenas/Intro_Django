ea3935f5d36dfeb6506c7afa0448e3761020a700
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-02 10:42:07 -0500

    Creación y borrado de datos

--- modularizacion/comentarios/urls.py
+++ modularizacion/comentarios/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('test/', views.test, name='test'),
-    path('create/', views.create, name='create')
+    path('create/', views.create, name='create'),
+    path('delete/', views.delete, name='delete'),
 ]
 
