ac911ce51b29622f53afafd5499643eeecf35db5
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-05 13:31:33 -0500

    Estableciendo relación one to one

--- relaciones/onetoone/views.py
+++ relaciones/onetoone/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Place, Restaurant
 
 def create(request):
-  # TODO Crear elementos
-  return HttpResponse('Datos creados')
+  place = Place(name='Lugar 1', address='Calle Demo')
+  place.save()
+
+  restaurant = Restaurant(place=place, number_of_employees=8)
+  restaurant.save()
+
+  return HttpResponse(restaurant.place.name)
