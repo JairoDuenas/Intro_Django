cafcf45b53a561c26a828cf12749df76c978f2d2
JairoDuenas <jairoduenas.ing@gmail.com>
2022-12-06 14:03:55 -0500

    Estructura basica de la base de datos

--- practica_modelos/company/models.py
+++ practica_modelos/company/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+  id_number = models.CharField(max_length=10, blank=False, null=False)
+  name = models.CharField(max_length=30, blank=False, null=False)
+  last_name = models.CharField(max_length=30, blank=False, null=False)
+  email = models.EmailField(max_length=30, blank=False, null=False)
+  address = models.CharField(max_length=50, blank=False, null=False)
+
+  def __str__(self):
+    return self.name
+
+class Job(models.Model):
+  title = models.CharField(max_length=15, blank=False, null=False)
+  description = models.TextField(null=True)
+
+  def __str__(self):
+    return self.title
+
+class Salary(models.Model):
+  amount = models.IntegerField(blank=False, null=False)
+  extra_december = models.BooleanField(default=False)
+  extra_june = models.BooleanField(default=False)
+
+  def __str__(self):
+    return self.amount
+
+class Place(models.Model):
+  name = models.CharField(max_length=30, blank=False, null=False)
+  address = models.CharField(max_length=50, blank=False, null=False)
+  zip_code = models.CharField(max_length=10, blank=False, null=False)
+
+  def __str__(self):
+    return self.name
+
+class Location(models.Model):
+  name = models.CharField(max_length=30, blank=False, null=False)
+
+  def __str__(self):
+    return self.name
+
+class Country(models.Model):
+  name = models.CharField(max_length=15, blank=False, null=False)
+  country_code = models.CharField(max_length=10, blank=False, null=False)
+
+  def __str__(self):
+    return self.name
